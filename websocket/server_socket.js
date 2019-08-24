b804c46f8c71393236cbdddc8b6c1f63bee693c1
Horace Velmont <dragond7@naver.com>
2019-08-24 13:54:11 +0900

    add sock test

--- websocket/server_socket.js
+++ websocket/server_socket.js
@@ -1,33 +1,36 @@
 //socket.io模块
 var Blogs = require('../models/blogs');
 
-function serverSocket(server){
-	var io = require('socket.io')(server);
-	io.on('connection',function(socket){
-		socket.on('subscribe',function(data){
-			//console.log(data);
-			data.rooms.forEach(function(ele){
-				socket.join(ele);
-			});
-		});
-		socket.on('add_comment',function(data){
-			console.log(data);
-			var blogId = data.blogId;
-			var commenter = data.author;
-			var comment = data.comment;
-			var date = data.date;
-			Blogs.findById(blogId,function(err,blogs){
-				if(err) console.log(err);
-				socket.broadcast.to(blogId).emit('render_comment',{
-					date: date,
-					commenter:commenter,
-					blogTitle: blogs[0].title,
-					blogId: blogId,
-					comment: comment
-				});
-			});
-		});
-	});
+function serverSocket(server) {
+  var io = require('socket.io')(server);
+  io.on('connection', function (socket) {
+    socket.on('hello', function (data) {
+      socket.emit('hello. I am hyobin bot. I like jjam bbong. You said : ' + data.msg);
+    });
+    socket.on('subscribe', function (data) {
+      //console.log(data);
+      data.rooms.forEach(function (ele) {
+        socket.join(ele);
+      });
+    });
+    socket.on('add_comment', function (data) {
+      console.log(data);
+      var blogId = data.blogId;
+      var commenter = data.author;
+      var comment = data.comment;
+      var date = data.date;
+      Blogs.findById(blogId, function (err, blogs) {
+        if (err) console.log(err);
+        socket.broadcast.to(blogId).emit('render_comment', {
+          date: date,
+          commenter: commenter,
+          blogTitle: blogs[0].title,
+          blogId: blogId,
+          comment: comment
+        });
+      });
+    });
+  });
 };
 
 module.exports = serverSocket;
